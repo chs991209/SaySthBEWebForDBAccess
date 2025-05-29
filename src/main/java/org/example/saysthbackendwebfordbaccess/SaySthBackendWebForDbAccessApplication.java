855f384d08b042da819738bf3e92c3576c103015
ChoiHs <chs991209@gmail.com>
2025-05-29 21:10:09 +0900

    [ADD] Initialization w/ Maven, docker compose Can be modified later

---
+++ src/main/java/org/example/saysthbackendwebfordbaccess/SaySthBackendWebForDbAccessApplication.java
@@ -0,0 +1,13 @@
+package org.example.saysthbackendwebfordbaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaySthBackendWebForDbAccessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaySthBackendWebForDbAccessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/saysthbackendwebfordbaccess/SaySthBackendWebForDbAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.saysthbackendwebfordbaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaySthBackendWebForDbAccessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
